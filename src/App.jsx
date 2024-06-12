29adedfa51120d14b82d3a9140438d155df915ba
jiasenwqbr <jasonjiasen@gmail.com>
2024-06-12 11:07:18 +0800

    delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
-import "./App.css";
+const App = ()=>{
+  return (
+    <main className="relative">
+      {/* <Nav></Nav> */}
+      Nav
+      <section className="xl:padding-l wide:padding-r padding-b">
+        {/* <Hero></Hero> */}Hero
+      </section>
+      <section className="padding">
+        {/* <PopularProducts/> */}
+        PopularProducts
+      </section>
 
-function App() {
-  return <h1 className="my-4 text-4xl font-bold">Hello world!</h1>;
+      <section className="padding">
+        {/* <SuperQuality/> */}
+        SuperQuality
+      </section>
+
+      <section className='padding-x py-10'>
+        {/* <Services /> */}
+        Services
+      </section>
+      <section className='padding'>
+        {/* <SpecialOffer /> */}
+        SpecialOffer
+      </section>
+      <section className='bg-pale-blue padding'>
+        {/* <CustomerReviews /> */}
+        CustomerReviews
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        {/* <Subscribe /> */}
+        Subscribe
+      </section>
+      <section className=' bg-black padding-x padding-t pb-8'>
+        {/* <Footer /> */}
+        Footer
+      </section>
+
+
+    </main>
+  )
 }
 
 export default App;
