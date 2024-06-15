5bc60ea03728e3771c627415dcdadff6a72a0a39
jiasenwqbr <jasonjiasen@gmail.com>
2024-06-15 17:30:50 +0800

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,35 @@
 import Nav from "./components/Nav";
+import {Hero, PopularProducts,SuperQuality,Services,SpecialOffer,CustomerReviews,Subscribe,Footer} from "./sections"
 const App = ()=>{
   return (
     <main className="relative">
       <Nav></Nav>
       <section className="xl:padding-l wide:padding-r padding-b">
-        {/* <Hero></Hero> */}Hero
+        <Hero></Hero>
       </section>
       <section className="padding">
-        {/* <PopularProducts/> */}
-        PopularProducts
+        <PopularProducts />
       </section>
 
       <section className="padding">
-        {/* <SuperQuality/> */}
-        SuperQuality
+        <SuperQuality/>
       </section>
 
       <section className='padding-x py-10'>
-        {/* <Services /> */}
-        Services
+        <Services />
       </section>
       <section className='padding'>
-        {/* <SpecialOffer /> */}
-        SpecialOffer
+        <SpecialOffer />
       </section>
       <section className='bg-pale-blue padding'>
-        {/* <CustomerReviews /> */}
-        CustomerReviews
+        <CustomerReviews />
       </section>
       <section className='padding-x sm:py-32 py-16 w-full'>
-        {/* <Subscribe /> */}
-        Subscribe
+        <Subscribe />
       </section>
       <section className=' bg-black padding-x padding-t pb-8'>
-        {/* <Footer /> */}
-        Footer
+        <Footer />
       </section>
-
-
     </main>
   )
 }
